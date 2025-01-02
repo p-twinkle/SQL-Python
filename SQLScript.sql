76425604935bcea7556281d1690fc0d60fee9147
Twinkle Panda <ptwinkle336@gmail.com>
2025-01-02 14:11:17 -0600

    Update SQLScript.sql

--- SQLScript.sql
+++ SQLScript.sql
@@ -7,3 +7,14 @@ FROM table_name;
 SELECT ROW_NUMBER() OVER (ORDER BY colname) AS ROW_NUM
 FROM table_name;
 
+-- SQL(Int)
+SELECT c.city_name, prod.product_name
+, ROUND(CAST(SUM(line_total_price) AS FLOAT),2)
+FROM city c
+INNER JOIN customer cust ON c.id = cust.city_id
+INNER JOIN invoice inv ON inv.customer_id = cust.id
+INNER JOIN invoice_item itm ON inv.id = itm.invoice_id
+INNER JOIN product prod ON prod.id = itm.product_id
+GROUP BY c.city_name, prod.product_name
+ORDER BY SUM(line_total_price) DESC
+;
