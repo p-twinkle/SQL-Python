4d591b9b1b9a7b072812f446f10ee33fd08a864f
Twinkle Panda <ptwinkle336@gmail.com>
2025-05-17 20:03:42 -0500

    Update SQLScript.sql

--- SQLScript.sql
+++ SQLScript.sql
@@ -1,13 +1,15 @@
-
--- Median of a column --------------------------------------------------
+----------------------------------------------------------------------------------------------
+-- Median of a column 
 SELECT ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY (colname)),4) AS Median
-FROM table_name;
-
--- Assign Row Number -------------------------------------------------------
+FROM table_name
+;
+----------------------------------------------------------------------------------------------
+-- Assign Row Number 
 SELECT ROW_NUMBER() OVER (ORDER BY colname) AS ROW_NUM
-FROM table_name;
-
--- SQL(Int)---------------------------------------------------------------
+FROM table_name
+;
+----------------------------------------------------------------------------------------------
+-- SQL(Int)
 SELECT c.city_name, prod.product_name
 , ROUND(CAST(SUM(line_total_price) AS FLOAT),2)
 FROM city c
@@ -18,8 +20,8 @@ INNER JOIN product prod ON prod.id = itm.product_id
 GROUP BY c.city_name, prod.product_name
 ORDER BY SUM(line_total_price) DESC
 ;
-
--- 3-day Rolling average --------------------------------------------------
+----------------------------------------------------------------------------------------------
+-- 3-day Rolling average 
 SELECT user_id, tweet_date
 , ROUND(AVG(tweet_count) OVER (
         PARTITION BY user_id 
@@ -28,3 +30,4 @@ SELECT user_id, tweet_date
         ) ,2) rolling_avg_3rd
 FROM tweets
 ;
+----------------------------------------------------------------------------------------------
