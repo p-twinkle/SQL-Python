75b735ba9f825b7587ed424d49657a37cfb9fd38
Twinkle Panda <ptwinkle336@gmail.com>
2025-07-29 16:52:57 -0500

    Update SQLScript.sql

--- SQLScript.sql
+++ SQLScript.sql
@@ -287,8 +287,84 @@ GROUP BY STR_ID, period_state
 ORDER BY MIN(date) 
 
 ----------------------------------------------------------------------------------------------
+/*
+Input: 
+Visits table:
++---------+------------+
+| user_id | visit_date |
++---------+------------+
+| 1       | 2020-01-01 |
+| 2       | 2020-01-02 |
+| 12      | 2020-01-01 |
+| 19      | 2020-01-03 |
+| 1       | 2020-01-02 |
+| 2       | 2020-01-03 |
+| 1       | 2020-01-04 |
+| 7       | 2020-01-11 |
+| 9       | 2020-01-25 |
+| 8       | 2020-01-28 |
++---------+------------+
+Transactions table:
++---------+------------------+--------+
+| user_id | transaction_date | amount |
++---------+------------------+--------+
+| 1       | 2020-01-02       | 120    |
+| 2       | 2020-01-03       | 22     |
+| 7       | 2020-01-11       | 232    |
+| 1       | 2020-01-04       | 7      |
+| 9       | 2020-01-25       | 33     |
+| 9       | 2020-01-25       | 66     |
+| 8       | 2020-01-28       | 1      |
+| 9       | 2020-01-25       | 99     |
++---------+------------------+--------+
+Output: 
++--------------------+--------------+
+| transactions_count | visits_count |
++--------------------+--------------+
+| 0                  | 4            |
+| 1                  | 5            |
+| 2                  | 0            |
+| 3                  | 1            |
++--------------------+--------------+
+Explanation: The chart drawn for this example is shown above.
+* For transactions_count = 0, The visits (1, "2020-01-01"), (2, "2020-01-02"), (12, "2020-01-01") and (19, "2020-01-03") did no transactions so visits_count = 4.
+* For transactions_count = 1, The visits (2, "2020-01-03"), (7, "2020-01-11"), (8, "2020-01-28"), (1, "2020-01-02") and (1, "2020-01-04") did one transaction so visits_count = 5.
+* For transactions_count = 2, No customers visited the bank and did two transactions so visits_count = 0.
+* For transactions_count = 3, The visit (9, "2020-01-25") did three transactions so visits_count = 1.
+* For transactions_count >= 4, No customers visited the bank and did more than three transactions so we will stop at transactions_count = 3
+*/
 
+WITH ADS AS
+(
+    SELECT v.*, amount
+    FROM visits v 
+    LEFT JOIN transactions t
+    ON v.user_id = t.user_id
+    AND v.visit_date = t.transaction_date
+)
+, TX AS
+(
+    SELECT user_id, visit_date, COUNT(amount) transactions_count
+    FROM ADS 
+    GROUP BY 1,2
+)
+, GS AS 
+(
+SELECT GENERATE_SERIES(0, MAX(transactions_count)) transactions_count
+FROM TX
+)
 
+SELECT 
+    GS.transactions_count, COALESCE(visits_count, 0) visits_count
+FROM GS 
+LEFT JOIN 
+    (
+    SELECT transactions_count, COUNT(visit_date) visits_count
+    FROM TX
+    GROUP BY 1
+    ) vc
+USING(transactions_count)
+ORDER BY 1
 
 
 
