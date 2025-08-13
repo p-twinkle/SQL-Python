c246b438d252411c4d34bcbedfa9272309e36aef
Twinkle Panda <ptwinkle336@gmail.com>
2025-08-13 14:58:10 -0500

    Update BigQuery.sql

--- BigQuery.sql
+++ BigQuery.sql
@@ -11,9 +11,14 @@ WHERE rn = 1
 ;
 
 -- BigQuery
+/*
+This is BigQuery-specific syntax (also in Snowflake) that filters after window functions are computed.
+It’s like putting the window function in a WHERE clause, except WHERE can’t directly use window functions.
+*/
 SELECT * 
 FROM t
 QUALIFY ROW_NUMBER() OVER(PARTITION BY business_key ORDER BY updated_at DESC) = 1
 ;
+
 ----------------------------------------------------------------------------------------------------------------------------------
 
